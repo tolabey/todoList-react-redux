2075693a2e475c6b75ee887864bd35f0541287e2
tolabey <tolamusti@gmail.com>
2018-04-13 10:44:20 +0300

    clear master

--- src/action/action.js
+++
@@ -1,8 +0,0 @@
-import I from "immutable";
-
-export function singleDataAction(type, payload) {
-  return {
-    type,
-    payload: I.fromJS(payload)
-  };
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./css/main.css";
-import App from "./components/App.js";
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
